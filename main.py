715c1d0f8fb6096fbd28044bfd2226c3c829ef1e
hchan <frqk3624@gmail.com>
2020-10-05 18:48:36 +0900

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import getpass
+from bs4 import BeautifulSoup
+
+email = input('Enter your email: ')
+password = getpass.getpass('Enter your password: ')
+
+login_url = 'https://id.jobcan.jp/users/sign_in'
+session = requests.session()
+response = session.get(login_url)
+bs = BeautifulSoup(response.text, 'html.parser')
+
+authenticity = bs.find(attrs={'name':'authenticity_token'}).get('value')
+cookie = response.cookies
+
+print(authenticity)
+
+info = {
+    "authenticity_token": authenticity,
+    "user[email]": email,
+    "user[password]": password,
+}
+
+res = session.post(login_url, data=info, cookies=cookie)
+print(res.text)
